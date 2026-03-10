9161c1edcdd05acc80c0dc4f9db1b8708bbe895e
Tom <150679896+cqptomii@users.noreply.github.com>
2026-03-10 22:04:13 +0100

    feat: Update Registry from the ECS system to use EntityManager and ComponentStorage

--- src/engine/ecs/registry.hpp
+++ src/engine/ecs/registry.hpp
@@ -7,6 +7,7 @@
 
 #include <cstdint>
 #include "entity.hpp"
+#include "entity_manager.hpp"
 #include "component_storage.hpp"
 #include <unordered_map>
 #include <memory>
@@ -15,96 +16,63 @@
 
 class Registry
 {
-    std::uint32_t next_entity_id;
+    EntityManager entities;
 
     // Map within a component type id and an array of entity related to a component
-    std::unordered_map<std::uint32_t, std::unique_ptr<IComponentStorage>> componentsArrays;
-
-    template<typename T>
-    ComponentStorage<T> get_array()
-    {
-        const auto component_index = std::type_index(typeid(T));
-        assert(this->componentsArrays.contains(component_index.hash_code()));
-        return static_cast<ComponentStorage<T>*>(this->componentsArrays[component_index.hash_code()].get());
-    }
-
-    template<typename T>
-    bool has_component(Entity e)
-    {
-        const auto component_index = std::type_index(typeid(T));
-
-        if (!this->componentsArrays.contains(component_index.hash_code()))
-        {
-            return false;
-        }
-
-        auto array = static_cast<ComponentStorage<T>*>(this->componentsArrays[component_index.hash_code()].get());
-
-        return array->contains(e);
-    }
-
-
-
+    std::unordered_map<std::type_index, std::unique_ptr<IComponentStorage>> pools;
 
 public:
-    Registry() : next_entity_id(1) {}
-
+    Registry() = default;
     Entity create_entity()
     {
-        return {this->next_entity_id++, 0};
+        return this->entities.create();
     }
 
     template<typename T>
-    void addComponent(Entity entity, T component)
+    ComponentStorage<T>& storage()
     {
-        const auto component_index = std::type_index(typeid(T));
-        if (!this->componentsArrays.contains(component_index.hash_code()))
+        const auto storage_index = std::type_index(typeid(T));
+        if (!this->pools.contains(storage_index))
         {
-            this->componentsArrays[component_index.hash_code()] = std::make_unique<ComponentStorage<T>>();
+            this->pools[storage_index] = std::make_unique<ComponentStorage<T>>();
         }
 
-        auto array = static_cast<ComponentStorage<T>*>(this->componentsArrays[component_index.hash_code()].get());
-        array->insert(entity, component);
+        return static_cast<ComponentStorage<T>&>(*this->pools[storage_index]);
     }
 
     template<typename T>
-    void removeComponent(Entity entity)
+    void add(Entity entity, T component)
     {
-        const auto component_index = std::type_index(typeid(T));
-        if (this->componentsArrays.contains(component_index.hash_code()))
-        {
-            auto array = static_cast<ComponentStorage<T>*>(this->componentsArrays[component_index.hash_code()].get());
-            array->remove(entity);
-        }
+        this->storage<T>().insert(entity, component);
     }
-
     template<typename T>
-    T& getComponent(Entity entity)
+    void remove(Entity entity)
     {
-        const auto component_index = std::type_index(typeid(T));
-        auto array = static_cast<ComponentStorage<T>*>(this->componentsArrays[component_index.hash_code()].get());
-        return array->get(entity);
+        this->storage<T>().remove(entity);
+    }
+    template<typename T>
+    T& get(Entity entity)
+    {
+        return this->storage<T>().get(entity);
     }
 
-    template<typename ...T>
-    std::vector<Entity> view()
+    template<typename First, typename... Rest>
+    void view()
     {
-        std::vector<Entity> entities;
+        auto& first = storage<First>();
 
-        if constexpr (sizeof...(T) == 0) return entities;
-
-
-        ComponentStorage<T> array = this->get_array<T>();
-        for (const auto& [id, generation] : array.get_data())
+        for (auto& element : first.data())
         {
-            Entity e = {id, generation};
-            if ((this->has_component<T>(e) && ...)) {
-                entities.push_back(e);
+            Entity e = element.entity;
+
+            if ((storage<Rest>().contains(e) && ...))
+            {
+                run_system(
+                    first.get(e),
+                    storage<Rest>().get(e)...
+                );
             }
         }
-
-
-        return entities;
     }
 
 };
