48873baffb3415626d14922f8a30338981423973
Tom <150679896+cqptomii@users.noreply.github.com>
2026-03-13 00:24:40 +0100

    feat: Update Material classes to include parameter

--- src/engine/resources/material_instance.hpp
+++ src/engine/resources/material_instance.hpp
@@ -11,28 +11,49 @@
 
 class MaterialInstance
 {
-    ResourceHandle<MaterialInstance> material_resource;
-    std::unordered_map<std::string, MaterialParameter> overrides;
+    ResourceHandle<MaterialResource> material_resource;
+    std::unordered_map<uint32_t, MaterialParameter> overrides;
 
 public:
-    explicit MaterialInstance(const ResourceHandle<MaterialInstance> material_resource) : material_resource(material_resource) {}
+    explicit MaterialInstance(const ResourceHandle<MaterialResource> material_resource) : material_resource(material_resource) {}
     ~MaterialInstance() = default;
 
+    ResourceHandle<MaterialResource> get_material_resource() const
+    {
+        return this->material_resource;
+    }
+
+    void set_override(const uint32_t index, const MaterialParameter& parameter)
+    {
+        this->overrides[index] = parameter;
+    }
     void set_override(const std::string& name, const MaterialParameter& parameter)
     {
-        this->overrides[name] = parameter;
+        this->set_override(hash_string(name), parameter);
+    }
+    MaterialParameter* get_override(uint32_t id)
+    {
+        auto it = overrides.find(id);
+        if (it != overrides.end())
+            return &it->second;
+
+        return nullptr;
     }
     MaterialParameter* get_override(const std::string& name)
     {
-        const auto it = this->overrides.find(name);
-        if (it == this->overrides.end())
-            return nullptr;
-
-        return &it->second;
+        return this->get_override(hash_string(name));
+    }
+    void delete_override(const uint32_t index)
+    {
+        this->overrides.erase(index);
     }
     void delete_override(const std::string& name)
     {
-        this->overrides.erase(name);
+        this->delete_override(hash_string(name));
+    }
+    const std::unordered_map<uint32_t, MaterialParameter>& get_overrides() const
+    {
+        return this->overrides;
     }
 };
 
--- src/engine/resources/material_resource.hpp
+++ src/engine/resources/material_resource.hpp
@@ -8,9 +8,21 @@
 #include "resource_handle.hpp"
 #include "shader_resource.hpp"
 #include "texture_resource.hpp"
+#include "../src/engine/utils.hpp"
 #include <unordered_map>
 #include <variant>
 #include <glm/glm.hpp>
+
+enum class MaterialParameterType
+{
+    FLOAT,
+    INT,
+    VEC2,
+    VEC3,
+    VEC4,
+    MAT4,
+    TEXTURE
+};
 
 struct MaterialParameter
 {
@@ -21,29 +33,48 @@ struct MaterialParameter
 class MaterialResource
 {
     ResourceHandle<ShaderResource> shader;
-    std::unordered_map<std::string, MaterialParameter> parameters;
+    std::unordered_map<uint32_t, MaterialParameter> parameters;
 
 public:
-    explicit MaterialResource(const ResourceHandle<ShaderResource> shader) : shader(shader) {}
+    explicit MaterialResource(const ResourceHandle<ShaderResource> shader) : shader(shader){}
     ~MaterialResource() = default;
 
+    ResourceHandle<ShaderResource> get_shader() const
+    {
+        return this->shader;
+    }
+    void set_parameter(const uint32_t index, const MaterialParameter& parameter)
+    {
+        this->parameters[index] = parameter;
+    }
     void set_parameter(const std::string& name, const MaterialParameter& parameter)
     {
-        this->parameters[name] = parameter;
+        this->parameters[hash_string(name)] = parameter;
     }
-    MaterialParameter* get_parameter(const std::string& name)
+    MaterialParameter* get_parameter(const uint32_t index)
     {
-        const auto it = this->parameters.find(name);
+        const auto it = this->parameters.find(index);
         if (it != this->parameters.end())
         {
             return &it->second;
         }
-
         return nullptr;
+    }
+    MaterialParameter* get_parameter(const std::string& name)
+    {
+        return get_parameter(hash_string(name));
+    }
+    void delete_parameter(const uint32_t index)
+    {
+        this->parameters.erase(index);
     }
     void delete_parameter(const std::string& name)
     {
-        this->parameters.erase(name);
+        delete_parameter(hash_string(name));
+    }
+    const std::unordered_map<uint32_t, MaterialParameter>& get_parameters() const
+    {
+        return this->parameters;
     }
 };
 
