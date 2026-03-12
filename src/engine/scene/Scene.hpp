ae5d036063a4d2f8000a47ea63151d0f5ab8ef39
Tom <150679896+cqptomii@users.noreply.github.com>
2026-03-13 00:38:21 +0100

    feat: Init Scene Class

---
+++ src/engine/scene/Scene.hpp
@@ -0,0 +1,62 @@
+//
+// Created by tomfr on 12/03/2026.
+//
+
+#ifndef ENGINE_SCENE_HPP
+#define ENGINE_SCENE_HPP
+
+#include <glm/glm.hpp>
+#include <../src/engine/resources/resource_manager.hpp>
+#include <../src/engine/ecs/registry.hpp>
+
+class Scene{
+
+    // Resource Manager class which load each resource to use
+    ResourceManager& resource_manager;
+
+    // ECS System which contains each object in the scene
+    Registry registry;
+
+
+    // Light sources
+    // Cameras
+public:
+    explicit Scene(ResourceManager& resource_manager) : resource_manager(resource_manager){}
+
+    Registry& get_registry()
+    {
+        return this->registry;
+    }
+    ResourceManager& get_resource_manager() const
+    {
+        return this->resource_manager;
+    }
+    template<typename T>
+    void add_component(Entity e, T component)
+    {
+        this->registry.add(e, component);
+    }
+    template<typename T>
+    void remove_component(const Entity entity)
+    {
+        this->registry.remove<T>(entity);
+    }
+
+    Entity add_object()
+    {
+        return this->registry.create_entity();
+    }
+    void remove_object(const Entity entity)
+    {
+        this->registry.remove_entity(entity);
+    }
+
+    template<typename T>
+    T& get_component(const Entity entity)
+    {
+        return this->registry.get<T>(entity);
+    }
+    Entity& get_entity(glm::vec2 cursor_pos);
+};
+
+#endif //ENGINE_SCENE_HPP
