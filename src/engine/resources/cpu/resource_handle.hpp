fbb02d432db7f97a34346e870184cd618531fb48
Tom <150679896+cqptomii@users.noreply.github.com>
2026-06-29 18:01:50 +0200

    Start debugging Engine for include the event system

--- src/engine/resources/cpu/resource_handle.hpp
+++ src/engine/resources/cpu/resource_handle.hpp
@@ -1,23 +1,86 @@
-//
-// Created by tomfr on 09/03/2026.
-//
+/**
+ * @file resource_handle.hpp
+ * @author Tom FRAISSE
+ * @brief ResourceHandle is a class that represents a handle to a resource
+ * @version 0.1
+ * @date 2026-06-28
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 
 #ifndef ENGINE_RESOURCE_HANDLE_HPP
 #define ENGINE_RESOURCE_HANDLE_HPP
 #include <cstdint>
 #include <limits>
 
+
 template<typename T>
 class ResourceHandle
 {
-public:
+public:    
+    // INVALID_ID is used to represent an invalid resource handle
     static constexpr uint32_t INVALID_ID = std::numeric_limits<uint32_t>::max();
+    
+    // The id of the resource handle
+    uint32_t m_id = INVALID_ID;
 
-    uint32_t id = INVALID_ID;
+    /**
+     * @brief Construct a new Resource Handle object with the default value
+     * @details The default value is INVALID_ID
+     */
+    ResourceHandle() = default;
+    
+    /**
+     * @brief Copy constructor
+     * @details The copy constructor is used to copy a resource handle
+     * @param other The resource handle to copy
+     */
+    ResourceHandle(const ResourceHandle&) = default;
 
+    /**
+     * @brief Copy assignment operator
+     * @details The copy assignment operator is used to copy a resource handle
+     * @param other The resource handle to copy
+     * @return The resource handle
+     */
+    ResourceHandle& operator=(const ResourceHandle&) = default;
+
+    /**
+     * @brief Move constructor
+     * @details The move constructor is used to move a resource handle
+     * @param other The resource handle to move
+     */
+    ResourceHandle(ResourceHandle&& other) noexcept : m_id(other.m_id) { 
+        other.m_id = INVALID_ID; 
+    }
+
+    /**
+     * @brief Move assignment operator
+     * @details The move assignment operator is used to move a resource handle
+     * @param other The resource handle to move
+     * @return The resource handle
+     */
+    ResourceHandle& operator=(ResourceHandle&& other) noexcept {
+         m_id = other.m_id; 
+         other.m_id = INVALID_ID; 
+         return *this;
+    }
+    
+    /**
+     * @brief Destructor
+     * @details The destructor is used to destroy a resource handle
+     */
+    ~ResourceHandle() = default;
+
+    /**
+     * @brief Check if the resource handle is valid
+     * @details The check is done by comparing the id to INVALID_ID
+     * @return true if the resource handle is valid, false otherwise
+     */
     constexpr explicit operator bool() const noexcept
     {
-        return this->id != INVALID_ID;
+        return this->m_id != INVALID_ID;
     }
 };
 
--- src/engine/systems/event_system/event_category.hpp
+++ src/engine/systems/event_system/event_category.hpp
@@ -1,14 +1,35 @@
+/**
+ * @file event_category.hpp
+ * @author Tom FRAISSE
+ * @brief 
+ * @version 0.1
+ * @date 2026-06-28
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
 #ifndef EVENTCATEGORY_HPP
 #define EVENTCATEGORY_HPP
 
+/**
+ * @brief EventCategory is a enum that represents the category of an event
+ * @details The category is used to filter events
+ */
 enum class EventCategory
 {
     None = 0,
+
+    // Application category
     Application = 1 << 0,
+
+    // Input category
     Input = 1 << 1,
     Keyboard = 1 << 2,
     Mouse = 1 << 3,
-    MouseButton = 1 << 4
+    MouseButton = 1 << 4,
+
+    // Window category
     Window = 1 << 5
 };
 
