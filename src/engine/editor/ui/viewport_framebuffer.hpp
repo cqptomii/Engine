2e71f43c1fbba8f9657e8034aba0f72a5e53a170
Tom <150679896+cqptomii@users.noreply.github.com>
2026-07-29 17:04:18 +0200

    Add Comments for viewport_framebuffer é

--- src/engine/editor/ui/viewport_framebuffer.hpp
+++ src/engine/editor/ui/viewport_framebuffer.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file viewport_framebuffer.hpp
+ * @author Tom FRAISSE
+ * @brief Header file for the viewport framebuffer
+ * @brief 
+ * @version 0.1
+ * @date 2026-07-29
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #ifndef VIEWPORT_FRAMEBUFFER_HPP
 #define VIEWPORT_FRAMEBUFFER_HPP
 
@@ -6,36 +17,63 @@
 #include <algorithm>
 #include <iostream>
 
+/**
+ * @brief Class representing the viewport framebuffer
+ */
 class ViewportFramebuffer {
+    // Framebuffer ID
     GLuint framebuffer_id = 0;
+    // Color texture ID
     GLuint color_texture_id = 0;
+    // Depth renderbuffer ID
     GLuint depth_renderbuffer_id = 0;
+    
+    // Width & Height of the viewport framebuffer
     int width = 0;
     int height = 0;
 
+    /**
+     * @brief Destroy the viewport framebuffer
+     * 
+     * @return void
+     */
     void destroy() {
+        // Destroy the depth renderbuffer of the viewport framebuffer
         if (this->depth_renderbuffer_id != 0) {
             glDeleteRenderbuffers(1, &this->depth_renderbuffer_id);
             this->depth_renderbuffer_id = 0;
         }
 
+        // Destroy the color texture of the viewport framebuffer
         if (this->color_texture_id != 0) {
             glDeleteTextures(1, &this->color_texture_id);
             this->color_texture_id = 0;
         }
 
+        // Destroy the framebuffer of the viewport framebuffer
         if (this->framebuffer_id != 0) {
             glDeleteFramebuffers(1, &this->framebuffer_id);
             this->framebuffer_id = 0;
         }
 
+        // Set the width and height of the viewport framebuffer to 0
         this->width = 0;
         this->height = 0;
     }
 
+    /**
+     * @brief Create the color texture of the viewport framebuffer
+     * 
+     * @param texture_width The width of the color texture
+     * @param texture_height The height of the color texture
+     * @return void
+     */
     void create_color_texture(const int texture_width, const int texture_height) {
+        // Generate the color texture of the viewport framebuffer
         glGenTextures(1, &this->color_texture_id);
         glBindTexture(GL_TEXTURE_2D, this->color_texture_id);
+        
+        // Create the image of the color texture
         glTexImage2D(
             GL_TEXTURE_2D,
             0,
@@ -47,6 +85,8 @@ class ViewportFramebuffer {
             GL_UNSIGNED_BYTE,
             nullptr
         );
+        
+        // Create the parameters of the color texture
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -54,21 +94,38 @@ class ViewportFramebuffer {
         glBindTexture(GL_TEXTURE_2D, 0);
     }
 
+    /**
+     * @brief Create the depth renderbuffer of the viewport framebuffer
+     * 
+     * @param texture_width The width of the depth renderbuffer
+     * @param texture_height The height of the depth renderbuffer
+     * @return void
+     */
     void create_depth_renderbuffer(const int texture_width, const int texture_height) {
+        // Generate the depth renderbuffer of the viewport framebuffer
         glGenRenderbuffers(1, &this->depth_renderbuffer_id);
         glBindRenderbuffer(GL_RENDERBUFFER, this->depth_renderbuffer_id);
+        
+        // Set the image of the depth renderbuffer
         glRenderbufferStorage(
             GL_RENDERBUFFER,
             GL_DEPTH24_STENCIL8,
             texture_width,
             texture_height
         );
+        
+        // Set the parameters of the depth renderbuffer
         glBindRenderbuffer(GL_RENDERBUFFER, 0);
     }
 
 public:
     ViewportFramebuffer() = default;
 
+    /**
+     * @brief Destructor of the viewport framebuffer
+     * 
+     * @return void
+     */
     ~ViewportFramebuffer() {
         this->destroy();
     }
@@ -76,6 +133,13 @@ public:
     ViewportFramebuffer(const ViewportFramebuffer&) = delete;
     ViewportFramebuffer& operator=(const ViewportFramebuffer&) = delete;
 
+    /**
+     * @brief Resize the viewport framebuffer
+     * 
+     * @param new_width The new width
+     * @param new_height The new height
+     * @return void
+     */
     void resize(const int new_width, const int new_height) {
         const int clamped_width = std::max(1, new_width);
         const int clamped_height = std::max(1, new_height);
@@ -84,16 +148,22 @@ public:
             return;
         }
 
+        // Destroy the viewport framebuffer
         this->destroy();
 
+        // Set the width and height of the viewport framebuffer
         this->width = clamped_width;
         this->height = clamped_height;
 
+        // Create the color texture of the viewport framebuffer
         this->create_color_texture(this->width, this->height);
+        // Create the depth renderbuffer of the viewport framebuffer
         this->create_depth_renderbuffer(this->width, this->height);
 
+        // Generate the framebuffer of the viewport framebuffer
         glGenFramebuffers(1, &this->framebuffer_id);
         glBindFramebuffer(GL_FRAMEBUFFER, this->framebuffer_id);
+        // Attach the color texture to the framebuffer
         glFramebufferTexture2D(
             GL_FRAMEBUFFER,
             GL_COLOR_ATTACHMENT0,
@@ -101,6 +171,7 @@ public:
             this->color_texture_id,
             0
         );
+        // Attach the depth renderbuffer to the framebuffer
         glFramebufferRenderbuffer(
             GL_FRAMEBUFFER,
             GL_DEPTH_STENCIL_ATTACHMENT,
@@ -121,23 +192,51 @@ public:
         glViewport(0, 0, this->width, this->height);
     }
 
+    /**
+     * @brief Unbind the viewport framebuffer
+     * 
+     * @param default_width The default width
+     * @param default_height The default height
+     * @return void
+     */
     void unbind(const int default_width, const int default_height) const {
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glViewport(0, 0, default_width, default_height);
     }
 
+    /**
+     * @brief Get the color texture ID of the viewport framebuffer
+     * 
+     * @return GLuint The color texture ID of the viewport framebuffer
+     */
     [[nodiscard]] GLuint get_color_texture_id() const noexcept {
         return this->color_texture_id;
     }
 
+    /**
+     * @brief Get the width of the viewport framebuffer
+     * 
+     * @return int The width of the viewport framebuffer
+     */
     [[nodiscard]] int get_width() const noexcept {
         return this->width;
     }
 
+    /**
+     * @brief Get the height of the viewport framebuffer
+     * 
+     * @return int The height of the viewport framebuffer
+     */
     [[nodiscard]] int get_height() const noexcept {
         return this->height;
     }
 
+    /**
+     * @brief Check if the viewport framebuffer is valid
+     * 
+     * @return true If the viewport framebuffer is valid
+     * @return false If the viewport framebuffer is not valid
+     */
     [[nodiscard]] bool is_valid() const noexcept {
         return this->framebuffer_id != 0 && this->color_texture_id != 0;
     }
--- src/engine/editor/ui/viewport_layout.hpp
+++ src/engine/editor/ui/viewport_layout.hpp
@@ -1,8 +1,27 @@
+/**
+ * @file viewport_layout.hpp
+ * @author Tom FRAISSE
+ * @brief Header file for the viewport layout
+ * @brief 
+ * @version 0.1
+ * @date 2026-07-29
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #ifndef VIEWPORT_LAYOUT_HPP
 #define VIEWPORT_LAYOUT_HPP
 
 #include <glm/glm.hpp>
 
+/**
+ * @brief Struct representing the viewport layout
+ *
+ * @param render_width The width of the viewport
+ * @param render_height The height of the viewport
+ * @param visible Whether the viewport is visible
+ * @param hovered Whether the viewport is hovered
+ */
 struct ViewportLayout {
     int render_width = 0;
     int render_height = 0;
@@ -10,16 +29,39 @@ struct ViewportLayout {
     bool hovered = false;
 };
 
+/**
+ * @brief Struct representing the viewport client bounds
+ *
+ * @param x The x coordinate of the viewport client bounds
+ * @param y The y coordinate of the viewport client bounds
+ * @param width The width of the viewport client bounds
+ * @param height The height of the viewport client bounds
+ * 
+ */
 struct ViewportClientBounds {
     float x = 0.0f;
     float y = 0.0f;
     float width = 0.0f;
     float height = 0.0f;
 
+    /**
+     * @brief Check if the viewport client bounds is valid
+     * 
+     * @return true If the viewport client bounds is valid
+     * @return false If the viewport client bounds is not valid
+     */
     [[nodiscard]] bool is_valid() const noexcept {
         return this->width >= 1.0f && this->height >= 1.0f;
     }
 
+    /**
+     * @brief Check if the viewport client bounds contains a mouse position
+     * 
+     * @param mouse_x The x coordinate of the mouse position
+     * @param mouse_y The y coordinate of the mouse position
+     * @return true If the viewport client bounds contains the mouse position
+     * @return false If the viewport client bounds does not contain the mouse position
+     */
     [[nodiscard]] bool contains(const float mouse_x, const float mouse_y) const noexcept {
         return mouse_x >= this->x
             && mouse_x < this->x + this->width
@@ -28,18 +70,27 @@ struct ViewportClientBounds {
     }
 };
 
-inline glm::vec2 viewport_client_to_framebuffer(
-    const glm::vec2& client_mouse,
-    const ViewportClientBounds& bounds,
-    const glm::vec2& framebuffer_size
+/**
+ * @brief Convert a client mouse position to a framebuffer position
+ * 
+ * @param client_mouse The client mouse position
+ * @param bounds The viewport client bounds
+ * @param framebuffer_size The framebuffer size
+ * @return glm::vec2 The framebuffer position
+ */
+inline glm::vec2 viewport_client_to_framebuffer(const glm::vec2& client_mouse, const ViewportClientBounds& bounds, const glm::vec2& framebuffer_size
 ) {
+
+    // Check if the viewport client bounds is valid and contains the mouse position
     if (!bounds.is_valid() || !bounds.contains(client_mouse.x, client_mouse.y)) {
         return glm::vec2(-1.0f);
     }
 
+    // Convert the client mouse position to a local position
     const float local_x = client_mouse.x - bounds.x;
     const float local_y = client_mouse.y - bounds.y;
 
+    // Convert the local position to a framebuffer position
     return glm::vec2(
         local_x * framebuffer_size.x / bounds.width,
         local_y * framebuffer_size.y / bounds.height
